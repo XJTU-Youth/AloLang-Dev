876afed639e6b9d2031ae9cd9fcba9cb41d89b51
NaiveTomcat <tomdang@tsinghuamakerxian.cn>
2020-04-03 04:07:43 +0800

    Added file input codes

--- compiler/cppfront.cpp
+++ compiler/cppfront.cpp
@@ -12,17 +12,29 @@ using std::cout;
 using std::cerr;
 using std::endl;
 
+std::string input_file_name;
+std::string buff;
+
 int main(int argc, char* argv[])
 {
 	if (argc == 1){
-		cerr << argv[0] << ": fatal error: " << "no input files\ncompilation terminated\n";
+		cerr << argv[0] << ": fatal error: no input files\ncompilation terminated\n";
 		return 1;
-	} else {
+	} else if (argc > 2){
+		cerr << argv[0] << ": fatal error: too many arguments\ncompilation terminated\n";
+	}else {
 		std::string args[argc];
 		for (char i = 1; i < argc; i++)
 			args[i-1] = argv[i];
+		if (args[1]=="--help"||args[0]=="-h"){
+			cout << "Usage: " << argv[1] << "file\n";
+			return 0;
+		}else {
+			input_file_name = args[1];
+		}
+		fin.open(input_file_name);
+		std::getline (fin,buff,char(EOF));
+		//todo: add compile codes here
 	}
-
-	//todo: put something here.
 	return 0;
 }
