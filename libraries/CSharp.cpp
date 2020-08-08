5a68f71e16c51a5ac9ea9e61a9bdc23029e1578d
qyuqxun <2321759131@qq.com>
2020-08-08 23:06:10 +0800

    加了头文件，还没写

---
+++ libraries/CSharp.cpp
@@ -0,0 +1,19 @@
+#define CSharp
+#define Csharp
+#define csharp
+
+#include<cstdio>
+#include<cstdlib>
+#include<iostream>
+#include<algorithm>
+
+#ifdef WIN32
+
+#include<Windows.h>
+
+void csharp(string name)
+
+#else
+
+#include <sys/types.h>  
+#include <sys/stat.h> 
