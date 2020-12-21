f90748fd4aa806bdb8043d278d85bbe1cad87082
NaiveTomcat <tomdang@tsinghuamakerxian.cn>
2020-12-22 00:07:47 +0800

    Changed ascii table and made some modifications to test cases

---
+++ compiler/test/module.s
@@ -0,0 +1,27 @@
+	.text
+	.file	"test.ll"
+	.globl	main                            # -- Begin function main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# %bb.0:                                # %entrypoint
+	pushq	%rax
+	.cfi_def_cfa_offset 16
+	leaq	.L__unnamed_1(%rip), %rdi
+	callq	puts@PLT
+	xorl	%eax, %eax
+	popq	%rcx
+	.cfi_def_cfa_offset 8
+	retq
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+	.type	.L__unnamed_1,@object           # @0
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.L__unnamed_1:
+	.asciz	"hi world!\n"
+	.size	.L__unnamed_1, 11
+
+	.section	".note.GNU-stack","",@progbits
