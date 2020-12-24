910cb7ec586d863a154e512d875cf490b047f26f
uebian <bocheng.zou@outlook.com>
2020-12-24 22:48:45 +0800

    Add puts demo

--- compiler/ast/FunctionAST.cpp
+++ compiler/ast/FunctionAST.cpp
@@ -25,9 +25,27 @@ FunctionAST::~FunctionAST() {
 
 llvm::Function* FunctionAST::Codegen() {
 	llvm::Function *func = proto->Codegen();
+	//todo:调试用代码，请删除
+
 	llvm::BasicBlock *entry = llvm::BasicBlock::Create(*unit->context, "entry",
 			func);
 	builder->SetInsertPoint(entry);
+	if (proto->name == "testPuts") {
+		llvm::Value *helloWorld = builder->CreateGlobalStringPtr("just for debug!\n");
+		std::vector<llvm::Type*> putsargs;
+		putsargs.push_back(builder->getInt8Ty()->getPointerTo());
+		llvm::ArrayRef<llvm::Type*> argsRef(putsargs);
+		llvm::FunctionType *putsType = llvm::FunctionType::get(builder->getInt32Ty(), argsRef,
+				false);
+		//FunctionType *mainType = FunctionType::get(builder.getInt32Ty(), false);
+		//3)创建“函数调用”，而不是创建函数
+		//FunctionCallee mainFunc = mod->getOrInsertFunction("_alolang_4main",);
+		llvm::FunctionCallee putsFunc = unit->module->getOrInsertFunction("puts", putsType);
+
+		//7、调用函数（<#理解：通过createXXX创建出来的所有指令都在SetInsertPoint后面#>）
+		builder->CreateCall(putsFunc, helloWorld); //这是创建方法的指令
+
+	}
 	for (ExprAST *expr : body) {
 		expr->Codegen(builder);
 	}
