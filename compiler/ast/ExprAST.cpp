524305db97266bd1da0f9cae7fd04391507b9605
uebian <bocheng.zou@outlook.com>
2021-01-23 23:50:31 +0800

    Add VarExprAST

--- compiler/ast/ExprAST.cpp
+++ compiler/ast/ExprAST.cpp
@@ -22,39 +22,47 @@ ExprAST* ExprAST::ParseExpression(CompileUnit *unit) {
 				strtol(lastToken.tokenValue.c_str(), NULL, 10));
 	}
 	case tok_identifier: {
+		//函数调用或定义
 		std::string idName = lastToken.tokenValue;
 		unit->curTok = unit->next_tok();
-		if (unit->curTok.type != tok_syntax || unit->curTok.tokenValue != "(") {
-			std::cerr << "err2:非函数调用未实现" << std::endl;
-		}
-		std::vector<ExprAST*> args;
-		while (true) {
+		if (unit->curTok.type != tok_syntax) {
+			std::string valName = unit->curTok.tokenValue;
+			//定义
+		} else if (unit->curTok.tokenValue == "(") {
+			std::vector<ExprAST*> args;
+			while (true) {
+				unit->curTok = unit->next_tok();
+
+				if (unit->curTok.type == tok_syntax
+						&& unit->curTok.tokenValue == ")") {
+					break;
+				}
+				if (unit->curTok.type == tok_syntax
+						&& unit->curTok.tokenValue == ",") {
+					continue;
+				}
+
+				ExprAST *arg = ExprAST::ParseExpression(unit);
+				args.push_back(arg);
+				//todo:异常处理
+			}
+
 			unit->curTok = unit->next_tok();
+			if (unit->curTok.type != tok_syntax
+					|| unit->curTok.tokenValue != ";") {
+				std::cerr << "结尾的分号丢失？" << std::endl;
 
-			if (unit->curTok.type == tok_syntax
-					&& unit->curTok.tokenValue == ")") {
-				break;
 			}
-			if (unit->curTok.type == tok_syntax
-					&& unit->curTok.tokenValue == ",") {
-				continue;
-			}
-
-			ExprAST *arg = ExprAST::ParseExpression(unit);
-			args.push_back(arg);
-			//todo:异常处理
+			return new CallExprAST(unit, idName, args);
+		} else {
+			std::cerr << "err2:非函数调用或定义未实现" << std::endl;
 		}
-
-		unit->curTok = unit->next_tok();
-		if (unit->curTok.type != tok_syntax || unit->curTok.tokenValue != ";") {
-			std::cerr << "结尾的分号丢失？" << std::endl;
-
-		}
-		return new CallExprAST(unit, idName, args);
+		break;
 	}
 	default: {
 		CompileError e("非函数调用未实现");
 		throw e;
 	}
 	}
+	return nullptr;
 }
