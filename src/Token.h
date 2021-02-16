6f8f8b24e7c1f7c860c6df5ccbf661faa18fca93
NaiveTomcat <tomdang@tsinghuamakerxian.cn>
2021-02-16 20:56:57 +0800

    Added some token types

--- src/Token.h
+++ src/Token.h
@@ -20,7 +20,12 @@ enum TokenType {
     tok_type,
     tok_return,
     tok_return_type,
-    tok_syntax
+    tok_syntax,
+    tok_key_fun,
+    tok_key_op,
+    tok_key_literal,
+    tok_key_control,
+    tok_key_switch
 };
 
 class Token
