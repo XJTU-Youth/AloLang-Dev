417e66fa30b32c2e58b8959143251d5b9e17c19d
NaiveTomcat <tomdang@tsinghuamakerxian.cn>
2021-02-16 23:09:40 +0800

    Added test program for a+b

--- src/lib/testPuts.c
+++ src/lib/testPuts.c
@@ -3,3 +3,10 @@
 extern void _alolang_8testPuts(void) { puts("AloLang Test Function"); }
 
 extern void _alolang_12testPrintInt3int(int a) { printf("%d\n", a); }
+
+extern int _alolang_10testGetInt()
+{
+    int a;
+    scanf("%d", &a);
+    return a;
+}
