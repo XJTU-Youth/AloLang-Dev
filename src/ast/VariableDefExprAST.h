a8c5bb03875f7db5312a1ad0f4b6872d0482ac15
uebian <bocheng.zou@outlook.com>
2021-02-20 21:17:17 +0800

    Add VariableDefExprAST

---
+++ src/ast/VariableDefExprAST.h
@@ -0,0 +1,29 @@
+/*
+ * VariableDefExprAST.h
+ *
+ *  Created on: Jan 23, 2021
+ *      Author: zbc
+ */
+
+#ifndef COMPILER_AST_VARIABLEDEFEXPRAST_H_
+#define COMPILER_AST_VARIABLEDEFEXPRAST_H_
+#include "ExprAST.h"
+
+class VariableDefExprAST : public ExprAST
+{
+  public:
+    VariableDefExprAST(CompileUnit *unit, const std::string &idName,
+                       const std::string &type, ExprAST *initValue,
+                       int argID = -1);
+    virtual ~VariableDefExprAST();
+    static VariableDefExprAST *ParseVar(CompileUnit * unit,
+                                        CodeBlockAST *codeblock,
+                                        std::string idName, std::string type);
+    llvm::Value *              Codegen(llvm::IRBuilder<> *builder);
+    std::string                idName;
+    llvm::Value *              alloca;
+    ExprAST *                  initValue;
+    int argID; //函数的参数号，函数内变量为-1
+};
+
+#endif /* COMPILER_AST_VARIABLEEXPRAST_H_ */
