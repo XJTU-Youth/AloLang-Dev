877d3346b26ed0e08c66ab02650c9e687f76d6bb
NaiveTomcat <tomdang@tsinghuamakerxian.cn>
2021-02-25 10:45:34 +0800

    edit demo.alo

--- test/perfcheck/demo.c
+++ test/perfcheck/demo.c
@@ -2,9 +2,14 @@
 
 int main(void)
 {
-    int a = 0;
-    while (a < 100000000)
-        a++;
-    printf("a = %d\n", a);
+    long long int a = 1;
+    long long int b = 1;
+    long long int c;
+    while (a < 4611686018427387903) {
+        c = a + b;
+        b = a;
+        a = c;
+    }
+    printf("a = %lld\n", a);
     return 0;
 }
