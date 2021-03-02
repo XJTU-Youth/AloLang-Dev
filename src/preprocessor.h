d71486e3936795209c634908f2a2dae736bdd1ee
NaiveTomcat <tomdang@tsinghuamakerxian.cn>
2021-03-02 20:59:25 +0800

    some random bug. idk what caused it

--- src/preprocessor.h
+++ src/preprocessor.h
@@ -4,9 +4,12 @@
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <vector>
+#include <tuple>
 #include <sstream>
 #include <string>
 
 std::string preProcess(const std::string &code, int cnt);
+// extern static std::vector<std::pair<std::string,int>>;
 
 #endif
