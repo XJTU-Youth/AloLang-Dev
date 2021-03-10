a9373ed2fc265ce35c3e76cf169fcbb5b9dbd962
NaiveTomcat <tomdang@tsinghuamakerxian.cn>
2021-03-10 20:40:11 +0800

    Added comment

--- src/preprocessor.h
+++ src/preprocessor.h
@@ -10,6 +10,7 @@
 #include <string>
 
 typedef std::pair<std::pair<std::string,int>,std::string> Tline;
+//外层：first是行所在文件名和行号，second是行内容
 
 std::vector<Tline> preProcess(const std::string &code, int cnt, std::string FN);
 
