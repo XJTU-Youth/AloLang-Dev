aa27c18bf0d2a8d93ba96e19157b896a54df32d2
uebian <bocheng.zou@outlook.com>
2021-03-10 17:09:26 +0800

    Fix names

--- src/lib/testrand.c
+++ src/lib/testrand.c
@@ -1,12 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-extern long long _alolang_10testRanInte(void)
-{
-    return rand();
-}
+extern long long _alolang_8testRand(void) { return rand(); }
 
-extern long long _alolang_8testTimee(void)
-{
-    return time(0);
-}
+extern long long _alolang_8testTime(void) { return time(0); }
