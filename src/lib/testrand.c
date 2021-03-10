fde5ef60c071566c6ec03da5518ff79f735633d3
NaiveTomcat <tomdang@tsinghuamakerxian.cn>
2021-03-10 17:47:36 +0800

    change the get time function to return ns

--- src/lib/testrand.c
+++ src/lib/testrand.c
@@ -1,12 +1,14 @@
+#include <inttypes.h>
+#include <math.h>
 #include <stdlib.h>
 #include <time.h>
 
-extern long long _alolang_10testRanInte(void)
-{
-    return rand();
-}
+extern long long _alolang_10testRanInte(void) { return rand(); }
 
 extern long long _alolang_8testTimee(void)
 {
-    return time(0);
+    struct timespec spec;
+    clock_gettime(0, &spec);
+    long long ns = spec.tv_sec * 1000000 + spec.tv_nsec;
+    return ns;
 }
