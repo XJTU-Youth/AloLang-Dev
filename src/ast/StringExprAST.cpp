9244b45e076f40e5d5810cdb02e61f915acdaef0
uebian <bocheng.zou@outlook.com>
2021-03-17 00:29:21 +0800

    Add some code related to the implementation of string

--- src/ast/StringExprAST.cpp
+++ src/ast/StringExprAST.cpp
@@ -7,12 +7,14 @@
 
 #include "StringExprAST.h"
 #include "../CompileError.hpp"
+#include "CallExprAST.h"
+#include "IntExprAST.h"
 #include "TypeAST.h"
 
 StringExprAST::StringExprAST(CompileUnit *unit, const std::string &str)
     : ExprAST(unit)
 {
-    // TODO Auto-generated constructor stub
+    this->str = str;
 }
 
 StringExprAST::~StringExprAST()
@@ -25,8 +27,41 @@ std::vector<llvm::Value *> StringExprAST::Codegen(llvm::IRBuilder<> *builder)
     type.clear();
     this->type.push_back(new TypeAST(unit, "string"));
     std::vector<llvm::Value *> result;
-    CompileError               e("字符串字面值未实现", source);
+    llvm::Constant *           const_array_4 =
+        llvm::ConstantDataArray::getString(unit->module->getContext(), str);
+    llvm::Function *function = builder->GetInsertBlock()->getParent();
+
+    llvm::IRBuilder<> sBuilder(&function->getEntryBlock(),
+                               function->getEntryBlock().begin());
+
+    llvm::StructType *sType =
+        (llvm::StructType *)(new TypeAST(unit, "array<char>"))->Codegen();
+    llvm::AllocaInst *alloca = sBuilder.CreateAlloca(sType);
+    std::string       dname =
+        "_alolang_C5arrayE4initP5array4chare3inte"; // todo:硬编码
+    llvm::Function *CalleeF = unit->module->getFunction(dname);
+    if (CalleeF == 0) {
+        CompileError e("Function " + dname +
+                       " not found in LLVM IR when building string");
+        throw e;
+    }
+    std::vector<llvm::Value *> args;
+    args.push_back(alloca);
+    args.push_back((new IntExprAST(unit, str.length()))->Codegen(builder)[0]);
+    builder->CreateCall(CalleeF, args);
+
+    dname   = "_alolang_C5arrayE4initP5array4chare3inte"; // todo:硬编码
+    CalleeF = unit->module->getFunction(dname);
+    if (CalleeF == 0) {
+        CompileError e("Function " + dname +
+                       " not found in LLVM IR when building string");
+        throw e;
+    }
+    args.clear();
+    args.push_back(alloca);
+    args.push_back((new IntExprAST(unit, str.length()))->Codegen(builder)[0]);
+    llvm::Value *ret = builder->CreateCall(CalleeF, args);
+    CompileError e("字符串字面值未实现");
     throw e;
-    // result.push_back(res);
     return result;
 }
