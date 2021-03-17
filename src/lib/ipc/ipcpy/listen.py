25ab9ef6a4c37a251f8c0edf2ca36e6b32a71038
zyb <zyb@DESKTOP-BL3TNQL.localdomain>
2021-03-18 01:06:25 +0800

    listen.py

---
+++ src/lib/ipc/ipcpy/listen.py
@@ -0,0 +1,32 @@
+import os
+import time
+
+def close(rd):
+    try:
+        os.close(rd)
+    except:
+        print("不存在指定管道")
+
+def listen(pipename):
+    try:
+        rd = os.open(pipename, os.O_RDONLY)
+    except IOError:
+        print("不存在指定管道")
+    else:
+        fl = 0
+        a=''
+        while True:
+            s = os.read(rd, 128)
+            if (len(s) == 0):
+                fl+=1
+                time.sleep(1)
+            else:
+                fl == 0
+                a += s
+            if (fl == 3)
+                return a
+                
+            
+                
+            
+            
